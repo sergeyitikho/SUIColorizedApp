f18f3fd193c136063c2f066ae7962c3889bf1cea
John Doe <sergey@itikho.com>
2023-12-17 21:18:37 +0300

    Initial Commit

---
+++ SUIColorizedApp/SUIColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIColorizedAppApp.swift
+//  SUIColorizedApp
+//
+//  Created by John Doe on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUIColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
